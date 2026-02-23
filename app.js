14096bb1870622b2f9c4e3755f2658b577f0b4bc
Barana <baranapabasara@gmail.com>
2026-02-23 15:50:09 +0530

    exampe for web-server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 console.log("Hello from Node!");
 const fs = require('fs');
+const http = require('http');
 
 fs.readFile('file.txt', 'utf8', function (err, data) {
   if (err) throw err;
@@ -10,3 +11,11 @@ fs.writeFile('file.txt', 'Hello World!', function (err) {
   if (err) throw err;
   console.log('File saved!');
 });
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
+
+console.log("Server running at http://localhost:8080");
