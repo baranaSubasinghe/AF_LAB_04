2c81930161d2b1f795d1ddcb36c58abdde889154
Barana <baranapabasara@gmail.com>
2026-02-23 15:55:10 +0530

    https-request example

--- app.js
+++ app.js
@@ -19,3 +19,18 @@ http.createServer(function (req, res) {
 }).listen(8080);
 
 console.log("Server running at http://localhost:8080");
+
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+  let data = '';
+
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  resp.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+
+}).on('error', (err) => {
+  console.log("Error: " + err.message);
+});
